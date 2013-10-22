aac2feb75bb67dabc473fa3a4d1aa535058c6ca9
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2013-10-22 17:33:19 +0530

    Fixing UI Issues Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Bestsellerbooks::Application.routes.draw do
   
   root :to => 'scrappers#show_latest_books'
-  get "scrappers/show_latest_books"
-  get 'scrappers/price_details', to: "scrappers#price_details", as: 'price_details'
-  get 'scrappers/refresh_details', to: 'scrappers#refresh_details', as: 'refresh_details'
-  get "scrappers/show", to: "scrappers#show", as: "show"
-  get "scrappers/show_books_by_category", as: "show_books_by_category"
-  get "scrappers/find_book_price"
+  get "books/show_latest_books"
+  get 'books/price_details', to: "scrappers#price_details", as: 'price_details'
+  get 'books/refresh_details', to: 'scrappers#refresh_details', as: 'refresh_details'
+  get "books/show", to: "scrappers#show", to: 'scrappers#show', as: "show"
+  get "books/show_books_by_category", to: 'scrappers#show_books_by_category', as: "show_books_by_category"
+  get "books/find_book_price", to: 'scrappers#find_book_price'
   # resources 'scrappers' do
   #  get :autocomplete_book_detail_title, :on => :collection
   # end
