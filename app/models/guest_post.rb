17537c4897485da37cd45385c37878d3d246314f
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2013-10-31 17:39:51 +0530

    Removed uniqueness validation error on email for Guest Post

--- app/models/guest_post.rb
+++ app/models/guest_post.rb
@@ -3,5 +3,5 @@ class GuestPost < ActiveRecord::Base
 
   validates_presence_of :name, :email, :message
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
-                                      message: "address is invalid" }, uniqueness: true, unless: "email.blank?"
+                                      message: "address is invalid" }, unless: "email.blank?"
 end
